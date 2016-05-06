c5241de38b4ed8e498c20426c79d1c3195001d92
Jun Zheng <arthurjordan@gmail.com>
2016-05-06 16:56:28 +1000

    no full path

--- test.sh
+++ test.sh
@@ -4,6 +4,5 @@ echo "print our work directory `pwd`"
 
 echo "Before Running Mocha Test 16:38 -------------------"
 # source /Users/junzheng/Projects/juns-projects/mocha-buildkite/node_modules/mocha/bin/_mocha
-
-/Users/junzheng/Projects/juns-projects/mocha-buildkite/node_modules/mocha/bin/mocha tests --recursive --watch
+mocha tests --recursive --watch
 echo "After Running Mocha Test -------------------"
