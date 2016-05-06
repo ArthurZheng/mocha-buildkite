5cc5513371b8b78b2506b5c20b8fd2257ee135d0
Jun Zheng <arthurjordan@gmail.com>
2016-05-06 16:53:42 +1000

    added full path

--- test.sh
+++ test.sh
@@ -3,5 +3,7 @@ echo "running as `whoami`"
 echo "print our work directory `pwd`"
 
 echo "Before Running Mocha Test 16:38 -------------------"
-mocha tests --recursive --watch
+# source /Users/junzheng/Projects/juns-projects/mocha-buildkite/node_modules/mocha/bin/_mocha
+
+/Users/junzheng/Projects/juns-projects/mocha-buildkite/node_modules/mocha/bin/mocha tests --recursive --watch
 echo "After Running Mocha Test -------------------"
