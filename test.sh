598f2636571db25111dadff06a4610a0f1dbad26
Jun Zheng <arthurjordan@gmail.com>
2016-05-09 10:10:36 +1000

    updated test.sh

--- test.sh
+++ test.sh
@@ -4,5 +4,8 @@ echo "print our work directory `pwd`"
 
 echo "Before Running Mocha Test 16:38 -------------------"
 # source /Users/junzheng/Projects/juns-projects/mocha-buildkite/node_modules/mocha/bin/_mocha
-mocha tests --recursive --watch
+
+npm install 
+
+./node_modules/.bin/mocha tests --recursive --watch
 echo "After Running Mocha Test -------------------"
